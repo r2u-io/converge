7ff4febc112d43e99ed9290fe9a8751366f957c4
Giovanni Abeni <giovanni.abeni.santos@usp.br>
2022-06-21 17:32:30 -0300

    Removed ECBR2022 iframe to use components

--- src/pages/ecbr2022/index.tsx
+++ src/pages/ecbr2022/index.tsx
@@ -2,7 +2,72 @@ import React from 'react'
 
 import Head from 'next/head'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+  }
+
+  .wrapper {
+      overflow-x: hidden;
+      max-width: 100%;
+      max-width: 600px;
+      margin: 0 auto 0;
+      position: relative;
+      z-index: 0;
+  }
+
+  .wrapper .container {
+      text-align: center;
+      padding: 15px 0;
+      box-sizing: border-box;
+      background-color: #ffffff;
+      position: relative;
+      z-index: 3;
+  }
+
+  .wrapper .container img{
+      width: 100px;
+      height: auto;
+  }
+
+  .wrapper #converge-logo {
+      width: 100px;
+      height: auto;
+      margin-top: 10px;
+  }
+
+  .wrapper .container video {
+      width: 300px;
+      height: 300px;
+      max-width: 90%;
+      object-fit: contain;
+  }
+
+  .wrapper iframe {
+      border-radius: 0 !important;
+      margin: 0 auto 0;
+      width: 98%;
+      min-height: 500px;
+      position: relative;
+      top: -45px;
+      left: -3px;
+      z-index: 2;
+      border: none;
+  }
+
+  .wrapper #hidder {
+      width: 100%;
+      height: 105px;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background-color: #ffffff;
+
+  }
+`
 
 export const IFrame = styled.iframe`
   overflow: hidden;
@@ -23,15 +88,24 @@ const ECBR2022: React.FC = () => {
       <Head>
         <title>{t('pages.title.ecbr2022')}</title>
       </Head>
-      <IFrame
-        src='https://storage.googleapis.com/r2u-demos-bucket/social-commerce-nft/index.html'
-        title='Social Digital Commerce - NFTs POAP'
-        width='300'
-        height='150'
-        frameBorder='0'
-        scrolling='no'
-        allow='xr-spatial-tracking; display-capture; magnetometer; picture-in-picture; wake-lock; screen-wake-lock; vr; geolocation; microphone; camera; midi; encrypted-media; autoplay; fullscreen; gyroscope; accelerometer;'
-      />
+      <GlobalStyle />
+      <div className='wrapper'>
+        <div className='container'>
+          <img id='social-logo' src='/erc2022/social-digital-commerce-logo.svg'/>
+        </div>
+        <div className='container'>
+          <video width='250' height='250' controls autoPlay={true} muted loop>
+              <source src='/erc2022/nft-art.mp4' type='video/mp4'/>
+          </video>
+        </div>
+        <iframe
+            src='https://eovvoavx7w2.typeform.com/to/CSX5sXwK'
+            allowFullScreen={true}
+        ></iframe>
+        <div className='container' id='hidder'>
+            <img id='converge-logo' src='/erc2022/converge-logo.svg'/>
+        </div>
+      </div>
     </>
   )
 }
